ca9ba9aa3926858814446e2b686bb7fb2e8069fe
rmanivannanraviraj-GG <rmanivannanraviraj@gmail.com>
2025-08-15 19:25:20 +0530

    Update ggformula_WordHunter.py

--- ggformula_WordHunter.py
+++ ggformula_WordHunter.py
@@ -40,7 +40,7 @@ def translate_definitions(definitions, target_lang='ta'):
             progress_bar.progress((index + 1) / total_items, text=f"Translating... {index + 1}/{total_items}")
             return translated_text
         except Exception:
-            return "Translation failed." # Returning a message on failure
+            return "" # Returning empty string on failure
 
     with ThreadPoolExecutor(max_workers=10) as executor:
         results = list(executor.map(translate_with_progress, definitions, range(total_items)))
@@ -179,7 +179,7 @@ with st.container():
                     tamil_list = translate_definitions(definitions_to_translate)
                     df_export["Tamil"] = tamil_list
             else:
-                df_export["Tamil"] = "-"
+                df_export["Tamil"] = ""
 
             if lang_choice == "English Only":
                 df_view = df_export[["Word", "Word Type", "English"]]
