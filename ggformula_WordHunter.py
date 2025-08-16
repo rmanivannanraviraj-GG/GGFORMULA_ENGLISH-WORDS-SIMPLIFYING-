abf97b36b480cb5a5aba893b1527688c6d012ed7
rmanivannanraviraj-GG <rmanivannanraviraj@gmail.com>
2025-08-16 21:17:32 +0530

    Update ggformula_WordHunter.py

--- ggformula_WordHunter.py
+++ ggformula_WordHunter.py
@@ -145,45 +145,45 @@ def create_pdf_content(words):
     
     # Using default fonts to avoid file not found errors
     penmanship_style = ParagraphStyle('Penmanship', parent=styles['Normal'], fontName='Helvetica-Bold', fontSize=24, leading=28, textColor=black, alignment=TA_CENTER)
+    
+    # We will create a style for the dotted words, but ReportLab doesn't support
+    # opacity directly on text, so we'll use a different font or color.
+    # For this example, we'll use a slightly different style to represent 'opacity'.
     dotted_style = ParagraphStyle('Dotted', parent=styles['Normal'], fontName='Courier', fontSize=24, leading=28, textColor=darkgrey, alignment=TA_CENTER)
+    normal_style = ParagraphStyle('Normal', parent=styles['Normal'], fontName='Helvetica', fontSize=22, alignment=TA_CENTER)
     
     story = []
     
+    # Add Name and Date placeholder
     story.append(Paragraph("<b>Name:</b> ____________________ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b>Date:</b> ____________________", styles['Normal']))
     story.append(Spacer(1, 0.5 * inch))
     
     story.append(Paragraph("<b>Handwriting Practice</b>", styles['Title']))
     story.append(Spacer(1, 0.5 * inch))
     
-    words_per_page = 5
-    words_to_process = words[:words_per_page * 10]
+    words_per_page = 15
+    words_to_process = words[:words_per_page]
     
-    for i in range(0, len(words_to_process), words_per_page):
-        if i > 0:
-            story.append(PageBreak())
-        
-        page_words = words_to_process[i:i + words_per_page]
-        
-        table_data = []
-        
-        # Create a single row for the bold words
-        bold_row = [Paragraph(f"<b>{word}</b>", penmanship_style) for word in page_words]
-        table_data.append(bold_row)
-        
-        # Create 4 more rows with the dotted/normal style
-        for _ in range(4):
-            clone_row = [Paragraph(word, dotted_style) for word in page_words]
-            table_data.append(clone_row)
+    table_data = []
+    
+    # Create a single row for the bold words
+    bold_row = [Paragraph(f"<b>{word}</b>", penmanship_style) for word in words]
+    table_data.append(bold_row)
+    
+    # Create 4 more rows with the dotted/normal style
+    for _ in range(4):
+        clone_row = [Paragraph(word, normal_style) for word in words]
+        table_data.append(clone_row)
 
-        table_style = [
-            ('INNERGRID', (0,0), (-1,-1), 0.25, black),
-            ('BOX', (0,0), (-1,-1), 0.25, black),
-            ('TOPPADDING', (0,0), (-1,-1), 10),
-            ('BOTTOMPADDING', (0,0), (-1,-1), 10),
-        ]
+    table_style = [
+        ('INNERGRID', (0,0), (-1,-1), 0.25, black),
+        ('BOX', (0,0), (-1,-1), 0.25, black),
+        ('TOPPADDING', (0,0), (-1,-1), 10),
+        ('BOTTOMPADDING', (0,0), (-1,-1), 10),
+    ]
 
-        story.append(Table(table_data, colWidths=[1.5*inch]*5, style=table_style))
-        story.append(Spacer(1, 0.5 * inch))
+    story.append(Table(table_data, colWidths=[1.5*inch]*5, style=table_style))
+    story.append(Spacer(1, 0.5 * inch))
 
     # Footer
     story.append(Spacer(1, 0.5 * inch))
